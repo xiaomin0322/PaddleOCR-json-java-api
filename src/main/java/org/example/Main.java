2179502845550500b96015463ba47a33cd99e803
zzm <xiaomin0322@sina.com>
2022-10-20 10:47:25 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         // paddleocr_json 的可执行文件所在路径
-        String exePath = "path/to/executable";
+        String exePath = "C:\\my\\soft\\Umi-OCR.v1.3.0\\Umi-OCR\\PaddleOCR-json\\PaddleOCR_json.exe";
 
         // 可选的配置项
         Map<String, Object> arguments = new HashMap<>();
@@ -18,7 +18,7 @@ public class Main {
         try (Ocr ocr = new Ocr(new File(exePath), arguments)) {
 
             // 对一张图片进行 OCR
-            String imgPath = "path/to/img";
+            String imgPath = "C:\\Users\\Desktop\\微信截图_20221020103342.png";
             OcrResponse resp = ocr.runOcr(new File(imgPath));
 
             // 或者直接识别剪贴板中的图片
